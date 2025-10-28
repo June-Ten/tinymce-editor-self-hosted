0274cf7bb5f5c0a2d79d08945200d83641eab64d
wangqiling <wangqiling@wpooldata.com>
2025-10-28 10:54:25 +0800

    初始化时显示默认值

--- src/components/TinyMceEditor.vue
+++ src/components/TinyMceEditor.vue
@@ -24,11 +24,22 @@ export default {
   },
   data() {
     return {
-      editorId: this.generateUniqueId()
+      editorId: this.generateUniqueId(),
+      editorInstance: null
+    }
+  },
+  watch: {
+    value(newVal) {
+      if (this.editorInstance && this.editorInstance.getContent() !== newVal) {
+        this.editorInstance.setContent(newVal || '')
+      }
     }
   },
   mounted() {
     this.initEditor()
+  },
+  beforeDestroy() {
+    this.destroyEditor()
   },
   methods: {
     generateUniqueId() {
@@ -42,14 +53,64 @@ export default {
         selector: '#' + this.editorId,
         license_key: 'gpl',
         language: 'zh_CN',
-        plugins: 'print preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template code codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount textpattern noneditable help charmap quickbars emoticons',
+        skin: 'oxide',
+        content_css: 'default',
+        height: this.height,
+        placeholder: this.placeholder,
+        plugins: 'preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media code codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
         toolbar: 'undo redo | formatselect | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | blockquote | link image media table | charmap fullscreen preview | code',
         toolbar_mode: 'sliding',
         menubar: false,
         branding: false,
         statusbar: true,
+        resize: true,
+        elementpath: true,
+        quickbars_selection_toolbar: 'bold italic underline | link blockquote quickimage quicktable',
+        quickbars_insert_toolbar: 'quickimage quicktable',
+        contextmenu: 'link image table',
+        automatic_uploads: true,
+        file_picker_types: 'image',
+        paste_data_images: true,
+        images_upload_handler: async (blobInfo, progress) => {
+          return new Promise((resolve, reject) => {
+            const reader = new FileReader()
+            reader.onload = () => {
+              resolve(reader.result)
+            }
+            reader.onerror = () => {
+              reject('图片读取失败')
+            }
+            reader.readAsDataURL(blobInfo.blob())
+          })
+        },
+        setup: (editor) => {
+          this.editorInstance = editor
+          
+          // 监听内容变化，实现双向绑定
+          editor.on('input change keyup', () => {
+            const content = editor.getContent()
+            if (content !== this.value) {
+              this.$emit('input', content)
+            }
+          })
+        },
+        init_instance_callback: (editor) => {
+          console.log('初始化后', this.value)
+          if (this.value) {
+            editor.setContent(this.value)
+          }
+        }
       }
-      tinymce.init(config)
+      
+      tinymce.init(config).then(() => {
+        this.editorInstance = tinymce.get(this.editorId)
+      })
+    },
+    destroyEditor() {
+      if (this.editorInstance) {
+        this.editorInstance.destroy()
+        this.editorInstance = null
+      }
     }
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,7 +23,7 @@ export default {
   },
   data() {
     return {
-      content: '',
+      content: '<p>Hello, world!</p>',
     }
   }
 }
